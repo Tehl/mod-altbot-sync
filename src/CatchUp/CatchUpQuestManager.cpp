d22ee02774e72e42846526a754bad7aef865b491
Tehl <Tehl@users.noreply.github.com>
2026-07-25 21:45:02 +0100

    Sort quest list ready for processing

--- src/CatchUp/CatchUpQuestManager.cpp
+++ src/CatchUp/CatchUpQuestManager.cpp
@@ -49,8 +49,8 @@ void CatchUpQuestManager::SatisfyPreQuests(QuestIdSet& questSet)
             uint32 prevId = abs(*itr_j);
             if (questSet.find(prevId) == questSet.end())
             {
-                LOG_INFO("module", "[catchup] Quest {} {} needs pre-quest: {} {}", questId, quest->GetTitle(), prevId,
-                         sObjectMgr->GetQuestTemplate(prevId)->GetTitle());
+                LOG_TRACE("module", "[catchup] Quest {} {} needs pre-quest: {} {}", questId, quest->GetTitle(), prevId,
+                          sObjectMgr->GetQuestTemplate(prevId)->GetTitle());
                 preQuests.insert(prevId);
             }
             else
@@ -72,6 +72,44 @@ void CatchUpQuestManager::SatisfyPreQuests(QuestIdSet& questSet)
     {
         LOG_INFO("module", "[catchup] All pre-quests satisfied");
     }
+}
+
+bool CatchUpQuestManager::SortQuestsByLevelAndChain(uint32 questIdA, uint32 questIdB)
+{
+    Quest const* questA = sObjectMgr->GetQuestTemplate(questIdA);
+    Quest const* questB = sObjectMgr->GetQuestTemplate(questIdB);
+
+    if (std::find(questB->prevQuests.begin(), questB->prevQuests.end(), questIdA) != questB->prevQuests.end())
+    {
+        // A is a pre-quest of B
+        return true;
+    }
+
+    if (std::find(questA->prevQuests.begin(), questA->prevQuests.end(), questIdB) != questA->prevQuests.end())
+    {
+        // B is a pre-quest of A
+        return false;
+    }
+
+    if (questA->GetMinLevel() < questB->GetMinLevel())
+    {
+        return true;
+    }
+    else if (questB->GetMinLevel() < questA->GetMinLevel())
+    {
+        return false;
+    }
+
+    if (questA->GetQuestLevel() < questB->GetQuestLevel())
+    {
+        return true;
+    }
+    else if (questB->GetQuestLevel() < questA->GetQuestLevel())
+    {
+        return false;
+    }
+
+    return questIdA < questIdB;
 }
 
 CatchUpQuestManager::CatchUpQuestManager(Player* bot) : bot(bot) {}
@@ -104,7 +142,7 @@ void CatchUpQuestManager::AddPlayerQuests(Player* player)
             continue;
         }
 
-        LOG_INFO("module", "[catchup] Bot needs to complete player quest: {} {}", questId, quest->GetTitle());
+        LOG_TRACE("module", "[catchup] Bot needs to complete player quest: {} {}", questId, quest->GetTitle());
         requiredQuestIds.insert(questId);
     }
 }
@@ -134,14 +172,45 @@ void CatchUpQuestManager::AddClassQuests()
             continue;
         }
 
-        LOG_INFO("module", "[catchup] Bot needs to complete class quest: {} {}", questId, quest->GetTitle());
+        if (bot->IsQuestRewarded(questId))
+        {
+            LOG_TRACE("module", "[catchup] Bot already completed quest: {} {}", questId, quest->GetTitle());
+            continue;
+        }
+
+        LOG_TRACE("module", "[catchup] Bot needs to complete class quest: {} {}", questId, quest->GetTitle());
         requiredQuestIds.insert(questId);
     }
 }
 
 void CatchUpQuestManager::SatisfyPreQuests() { SatisfyPreQuests(requiredQuestIds); }
 
-void CatchUpQuestManager::CompleteQuests()
+CompleteQuestResult CatchUpQuestManager::CompleteQuests()
 {
     LOG_INFO("module", "[catchup] {} quests to complete", requiredQuestIds.size());
+
+    std::vector<uint32> questsToComplete{};
+    questsToComplete.reserve(requiredQuestIds.size());
+    questsToComplete.insert(questsToComplete.end(), requiredQuestIds.begin(), requiredQuestIds.end());
+
+    std::sort(questsToComplete.begin(), questsToComplete.end(), SortQuestsByLevelAndChain);
+
+    for (uint32 questId : questsToComplete)
+    {
+        CompleteQuestResult res = CompleteQuest(questId);
+        if (res != COMPLETE_ERR_OK)
+        {
+            return res;
+        }
+    }
+
+    return COMPLETE_ERR_OK;
+}
+
+CompleteQuestResult CatchUpQuestManager::CompleteQuest(uint32 questId)
+{
+    Quest const* quest = sObjectMgr->GetQuestTemplate(questId);
+    LOG_INFO("module", "[catchup] Ready to complete quest {} {}", questId, quest->GetTitle());
+
+    return COMPLETE_ERR_OK;
 }
--- src/CatchUp/CatchUpQuestManager.h
+++ src/CatchUp/CatchUpQuestManager.h
@@ -5,6 +5,12 @@
 
 typedef std::unordered_set<uint32> QuestIdSet;
 
+enum CompleteQuestResult : uint8
+{
+    COMPLETE_ERR_OK = 0,
+    COMPLETE_ERR_INVENTORY_FULL = 1,
+};
+
 class CatchUpQuestManager
 {
 public:
@@ -15,12 +21,15 @@ public:
     void AddPlayerQuests(Player* player);
     void AddClassQuests();
     void SatisfyPreQuests();
-    void CompleteQuests();
+    CompleteQuestResult CompleteQuests();
 
 private:
     static void SatisfyPreQuests(QuestIdSet& questSet);
+    static bool SortQuestsByLevelAndChain(uint32 questIdA, uint32 questIdB);
 
     static QuestIdSet classQuestIds;
+
+    CompleteQuestResult CompleteQuest(uint32 questId);
 
     Player* bot;
     QuestIdSet requiredQuestIds;
